299c9d24a1e97dc131596f9627186ef28a60ffbd
takurinton <takurinton@takurinton.dev>
2023-04-23 13:32:00 +0900

    add: other impl

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io::Read;
 use std::net::{TcpListener, TcpStream};
 use std::thread;
@@ -15,12 +16,10 @@ enum Method {
     // PATCH,
 }
 
-struct RequestHeader {}
-
 struct Request {
     method: Method,
     path: String,
-    headers: RequestHeader,
+    headers: HashMap<String, String>,
 }
 
 impl Request {
@@ -28,61 +27,91 @@ impl Request {
         Request {
             method: Method::GET, // TODO
             path: String::new(),
-            headers: RequestHeader {},
+            headers: HashMap::new(),
         }
     }
 
-    fn parse(&mut self, data: TcpStream) -> Self {
-        let mut buf = [0 as u8; 1024];
-        let mut stream = data;
+    fn parse(&mut self, mut stream: TcpStream) {
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer).unwrap();
+        let request = std::str::from_utf8(&buffer).unwrap();
 
-        stream.read(&mut buf).unwrap();
-        let mut i = 0;
-        let method = Request::get_method(self, &mut i, &buf);
-        let path = Request::get_path(self, &mut i, &buf);
+        let request_line_end = request.find("\r\n").unwrap();
+        let request_line = &request[..request_line_end];
+        let mut request_parts = request_line.split_whitespace();
 
-        return Request {
-            method,
-            path,
-            // TODO
-            headers: RequestHeader {},
-        };
-    }
-
-    fn get_method(&mut self, i: &mut usize, buf: &[u8]) -> Method {
-        let mut index = i.clone();
-        let mut method = String::new();
-        while buf[index] != b' ' {
-            method.push(buf[index] as char);
-            index += 1;
-        }
-
-        // どこまで進めたかを書き換える
-        *i = index + 1;
-
-        let m = match method.as_str() {
+        let method = String::from(request_parts.next().unwrap());
+        let method = match method.as_str() {
             "GET" => Method::GET,
             _ => panic!("Invalid HTTP method"),
         };
+        let path = String::from(request_parts.next().unwrap());
 
-        *i = index + 1;
+        self.method = method;
+        self.path = path;
 
-        self.method = m;
-        m
-    }
-
-    fn get_path(&mut self, i: &mut usize, buf: &[u8]) -> String {
-        let mut index = i.clone();
-        let mut path = String::new();
-
-        while buf[index] != b' ' {
-            path.push(buf[index] as char);
-            index += 1;
+        let headers_end = request.find("\r\n\r\n").unwrap();
+        let headers = &request[request_line_end + 2..headers_end];
+        for header in headers.lines() {
+            let mut header_parts = header.split(": ");
+            let header_name = header_parts.next().unwrap();
+            let header_value = header_parts.next().unwrap();
+            self.headers
+                .insert(String::from(header_name), String::from(header_value));
         }
-
-        self.path = path.clone();
-        path
     }
+
+    // fn parse(&mut self, data: TcpStream) -> Self {
+    //     let mut buf = [0 as u8; 1024];
+    //     let mut stream = data;
+
+    //     stream.read(&mut buf).unwrap();
+    //     let mut i = 0;
+    //     let method = Request::get_method(self, &mut i, &buf);
+    //     let path = Request::get_path(self, &mut i, &buf);
+
+    //     return Request {
+    //         method,
+    //         path,
+    //         // TODO
+    //         headers: HashMap::new(),
+    //     };
+    // }
+
+    // fn get_method(&mut self, i: &mut usize, buf: &[u8]) -> Method {
+    //     let mut index = i.clone();
+    //     let mut method = String::new();
+    //     while buf[index] != b' ' {
+    //         method.push(buf[index] as char);
+    //         index += 1;
+    //     }
+
+    //     // どこまで進めたかを書き換える
+    //     *i = index + 1;
+
+    //     let m = match method.as_str() {
+    //         "GET" => Method::GET,
+    //         _ => panic!("Invalid HTTP method"),
+    //     };
+
+    //     *i = index + 1;
+
+    //     self.method = m;
+    //     m
+    // }
+
+    // fn get_path(&mut self, i: &mut usize, buf: &[u8]) -> String {
+    //     let mut index = i.clone();
+    //     let mut path = String::new();
+
+    //     while buf[index] != b' ' {
+    //         path.push(buf[index] as char);
+    //         index += 1;
+    //     }
+
+    //     self.path = path.clone();
+    //     path
+    // }
 
     fn log(&self) {
         let method = match self.method {
@@ -90,6 +119,7 @@ impl Request {
         };
         println!("Method: {:?}", method);
         println!("Path: {}", self.path);
+        println!("Headers: {:?}", self.headers);
     }
 }
 
