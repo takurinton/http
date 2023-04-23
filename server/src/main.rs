e5f447d74d2495d73cf5c2259e4654745ef14aa5
takurinton <takurinton@takurinton.dev>
2023-04-23 13:13:27 +0900

    init

---
+++ server/src/main.rs
@@ -0,0 +1,114 @@
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+#[derive(Copy, Clone)]
+enum Method {
+    GET,
+    // POST,
+    // PUT,
+    // DELETE,
+    // HEAD,
+    // CONNECT,
+    // OPTIONS,
+    // TRACE,
+    // PATCH,
+}
+
+struct RequestHeader {}
+
+struct Request {
+    method: Method,
+    path: String,
+    headers: RequestHeader,
+}
+
+impl Request {
+    fn new() -> Request {
+        Request {
+            method: Method::GET, // TODO
+            path: String::new(),
+            headers: RequestHeader {},
+        }
+    }
+
+    fn parse(&mut self, data: TcpStream) -> Self {
+        let mut buf = [0 as u8; 1024];
+        let mut stream = data;
+
+        stream.read(&mut buf).unwrap();
+        let mut i = 0;
+        let method = Request::get_method(self, &mut i, &buf);
+        let path = Request::get_path(self, &mut i, &buf);
+
+        return Request {
+            method,
+            path,
+            // TODO
+            headers: RequestHeader {},
+        };
+    }
+
+    fn get_method(&mut self, i: &mut usize, buf: &[u8]) -> Method {
+        let mut index = i.clone();
+        let mut method = String::new();
+        while buf[index] != b' ' {
+            method.push(buf[index] as char);
+            index += 1;
+        }
+
+        // どこまで進めたかを書き換える
+        *i = index + 1;
+
+        let m = match method.as_str() {
+            "GET" => Method::GET,
+            _ => panic!("Invalid HTTP method"),
+        };
+
+        *i = index + 1;
+
+        self.method = m;
+        m
+    }
+
+    fn get_path(&mut self, i: &mut usize, buf: &[u8]) -> String {
+        let mut index = i.clone();
+        let mut path = String::new();
+
+        while buf[index] != b' ' {
+            path.push(buf[index] as char);
+            index += 1;
+        }
+
+        self.path = path.clone();
+        path
+    }
+
+    fn log(&self) {
+        let method = match self.method {
+            Method::GET => "GET",
+        };
+        println!("Method: {:?}", method);
+        println!("Path: {}", self.path);
+    }
+}
+
+fn main() {
+    let listener = TcpListener::bind("0.0.0.0:65535").unwrap();
+    println!("Server listening on port 65535");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move || {
+                    let mut request = Request::new();
+                    request.parse(stream);
+                    request.log();
+                });
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
+    drop(listener);
+}
